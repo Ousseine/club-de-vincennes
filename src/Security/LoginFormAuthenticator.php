4af34eabb57f58f68bd3f0514c5a0d3cd8b72a00
Ousseine <ousseinetech@gmail.com>
2021-05-09 11:30:47 +0200

    Adding finaly commit

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -71,7 +71,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
 
         if (!$user) {
             // fail authentication with a custom error
-            throw new CustomUserMessageAuthenticationException('Email could not be found.');
+            throw new CustomUserMessageAuthenticationException('L\'adresse email ou le mot de passe n\'est pas valide.');
         }
 
         return $user;
